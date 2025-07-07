e4a49ef8292ccbaccfad6cc2d0712f1cb317d5ec
svv98 <76674639+svv98@users.noreply.github.com>
2025-07-06 18:55:58 -0700

    added main src files, webpack config and package.json files

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { watchFile } = require("fs");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+        }),
+    ],
+    devtool: "eval-source-map",
+    devServer: {
+        watchFiles: ['./src/template.html'],
+    },
+    module: {
+        rules: [
+        {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+        },
+        ],
+    },
+};
