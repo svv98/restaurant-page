d0575914569a6f88b175094dc989be2d0775e638
svv98 <76674639+svv98@users.noreply.github.com>
2025-07-11 22:59:26 -0700

    11/7

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import "./styles.css";
 console.log('hola :)');
 console.log('hi');
+
+const menu = document.querySelector('.menuCenter');
+const infoModal = document.querySelector('#itemsInfo');
+
+menu.addEventListener('click', (event)=>{
+    if(event.target.closest(".itemName")){
+        infoModal.showModal();
+    }
+});
