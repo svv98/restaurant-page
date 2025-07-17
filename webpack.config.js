64982a4ee52583e45704c08f4e7e893c176eb2c1
svv98 <76674639+svv98@users.noreply.github.com>
2025-07-16 21:04:09 -0700

    finished About Page's CSS.
    Started and finished JS for Home Page and Menu Page.
    Started Info Modal JS.

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,10 @@ module.exports = {
                 test: /\.html$/i,
                 loader: "html-loader",
             },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: 'asset/resource',
+            },
         ],
     },
 };
